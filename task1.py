68a7f42915460a2a35c29ed7c580338aa6c402fd
ash2304L <167247407+ash2304L@users.noreply.github.com>
2024-04-17 19:29:12 +0100

    task1 completed
    Runs apache beam pipeline

---
+++ task1.py
@@ -0,0 +1,36 @@
+#Imports
+import apache_beam as beam
+from apache_beam.io import ReadFromText, WriteToText
+from apache_beam.options.pipeline_options import PipelineOptions
+
+#Functions for the pipeline
+def filter_transactions(transaction):
+    """
+    Function that will filter out transactions with transaction amounts lower than 20
+    and dates before 2010.
+    """
+    transaction_amount = float(transaction['transaction_amount'])
+    return transaction_amount > 20 and not transaction['timestamp'].startswith('200')
+
+def format_output(date_total):
+    """
+    Converts the tuples back into a dictionary
+    """
+    return f'{date_total[0]}, {date_total[1]}'
+
+#The Pipeline
+def run():
+    options = PipelineOptions()
+    with beam.Pipeline(options=options) as p:
+        transactions = (
+            p | 'ReadCSV' >> ReadFromText('gs://cloud-samples-data/bigquery/sample-transactions/transactions.csv', skip_header_lines=1)
+              | 'ParseCSV' >> beam.Map(lambda line: dict(zip(['timestamp', 'origin', 'destination', 'transaction_amount'], line.split(','))))
+              | 'FilterTransactions' >> beam.Filter(filter_transactions)
+              | 'MapToTuple' >> beam.Map(lambda transaction: (transaction['timestamp'][:10], float(transaction['transaction_amount'])))
+              | 'SumTotalByDate' >> beam.CombinePerKey(sum)
+              | 'FormatOutput' >> beam.Map(format_output)
+              | 'WriteToJSONLines' >> WriteToText('output/results', file_name_suffix='.jsonl.gz', shard_name_template='')
+        )
+
+if __name__ == '__main__':
+    run()
